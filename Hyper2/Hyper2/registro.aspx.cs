b642a2ce743d8a3cfc49b86f07fd15ea0f4598dd
JDLK7 <josesguay@gmail.com>
2016-05-09 22:23:44 +0200

    Cambios en .cs del registro.

--- Hyper2/Hyper2/registro.aspx.cs
+++ Hyper2/Hyper2/registro.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Hyper.EN;
+using System.Net.Mail;
 
 namespace Hyper2
 {
@@ -51,6 +53,91 @@ namespace Hyper2
 
             return r;
         }
+        
+        /*
+         * Método que comprueba si el email introducido es válido y si ya existe en la bbdd.
+         * Devuelve un booleano.
+         */
+        private bool check_email()
+        {
+            //Buscar en lista de email validos.
+            string email = textBox_email.Text;
+            if (!(email.Contains("@gmail.com") || email.Contains("@hotmail.com") || email.Contains("@hotmail.es")))
+            {
+                label_email.Text = "El email no es válido";
+                return false;
+            }
+            else
+            {
+                //Buscar en BBDD.
+            }
+        }
+        
+        /*
+         * Método que comprueba si el nombre de usuario introducido está disponible o ya esta en uso.
+         * En caso contrario se indica en un bajo el textBox correspondiente.
+         * Devuelve un booleano.
+         */ 
+        private bool check_username()
+        {
+            //Buscar en BBDD.
+        }
+        
+        /*
+         * Comprueba si la contraseña y la confirmación de la contraseña coinciden y si el tamaño 
+         * es el adecuado. Si no coinciden o se supera el tamaño máximo se indica en el label
+         * correspondiente. Devuelve un booleano.
+         */
+        private bool check_password()
+        {
+            if(textBox_password.Text.Length > 16 || textBox_passwordConf.Text.Length > 16)
+            {
+                label_password.Text = "No puede superar los 16 caracteres";
+                return false;
+            }
+            else
+            {
+                if (textBox_password.Text == textBox_passwordConf.Text)
+                {
+                    return true;
+                }
+                else
+                {
+                    label_password.Text = "Las contraseñas no coinciden";
+                    return false;
+                }
+            }
+        }
+
+        /*
+         * Envía un email de bienvenida al usuario que se acaba de registrar.
+         */
+        private void confirmation_email(string email, string firstName)
+        {
+            //Contraseña mail "molamasquemega"
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            MailMessage message = new MailMessage();
+            
+            try
+            {
+                MailAddress fromAddress = new MailAddress("noreply.hyperCloud@gmail.com");
+                MailAddress toAddress = new MailAddress(email);
+                message.Subject = "¡Bienvenido a Hyper!";
+                message.Body =
+                    "Hola " + firstName + ", te damos la bienvenida a Hyper, el mejor y más social sistema de almacenamiento en la nube. " +
+                    "Recuerda que si quieres puedes suscribirte a una de nuestras tarifas para tener aun más espacio.";
+                message.From = fromAddress;
+                message.To.Add(toAddress);
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new System.Net.NetworkCredential("noreply.hyperCloud@gmail.com", "molamasquemega");
+                smtpClient.Send(message);
+            }
+            catch(Exception ex)
+            {
+                //Error al enviar el mensaje.
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -58,16 +145,19 @@ namespace Hyper2
 
         protected void button_signIn_Click(object sender, EventArgs e)
         {
-            //Devuelve bool.
-            check_textBoxes();
-
-            //Buscar en lista de email validos.
-            string email = textBox_email.Text;
-            if (!(email.Contains("@gmail.com") || email.Contains("@hotmail.com") || email.Contains("@hotmail.es")))
+            if(check_textBoxes() && check_username() && check_email() && check_password())
             {
-                label_email.Text = "El email no es válido";
-            }
+                string  firstName = textBox_firstName.Text,
+                        lastName = textBox_lastName.Text,
+                        username = textBox_username.Text,
+                        email = textBox_email.Text,
+                        password = textBox_password.Text;
 
+                UserEN user = new UserEN(username,firstName,lastName,email,password);
+                user.Save();
+
+                confirmation_email(email, firstName);
+            }
         }
     }
 }
