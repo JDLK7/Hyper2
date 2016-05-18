6db4c8f4b3f6f94b2af415fbaf06b9b884577b45
JDLK7 <josesguay@gmail.com>
2016-05-18 17:22:06 +0200

    COMMIT BY EDDIE: Añadido el descargador de links de archivos.

---
+++ Hyper2/Hyper2/download.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Hyper2
+{
+    public partial class download : System.Web.UI.Page
+    {
+        private string path = @"C:\HyperDataFiles\Temporary\";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Request.QueryString["file"] != "")
+            {
+                FileInfo f = new FileInfo(path + Request.QueryString["file"]);
+                if (f.Exists)
+                {
+                    Download(path + Request.QueryString["file"]);
+                }
+            
+            }
+        }
+
+        private void Download(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+
+            if (fileInfo.Exists)
+            {
+                Response.Clear();
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + fileInfo.Name);
+                Response.AddHeader("Content-Length", fileInfo.Length.ToString());
+                Response.ContentType = "application/octet-stream";
+                Response.Flush();
+                Response.TransmitFile(fileInfo.FullName);
+                File.Delete(path);
+                Response.End();
+            }
+        }
+    }
+}
